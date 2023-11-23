ba74e8db6370a1bdb4079e522831c82afe1882bd
mottamx <cmottaa@gmail.com>
2023-11-23 13:54:35 -0600

    Added Booking and Menu models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(validators=[MaxValueValidator(6)])
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.title
