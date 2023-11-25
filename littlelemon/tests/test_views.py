82d664c0c93ea9c8973d4dec878e6c31145f813d
mottamx <cmottaa@gmail.com>
2023-11-25 01:53:17 -0600

    Added unit tests

--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -1,22 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from rest_framework.authtoken.models import Token
+
 from restaurant.views import Menu
 from restaurant.serializers import MenuSerializer
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APIClient
 
-class MenuViewTest(TestCase):
+class MenuViewTest(APITestCase):
     def setUp(self):
-        Menu.objects.create(title="Dish1", price=10.99, inventory=2)
-        Menu.objects.create(title="Dish2", price=15.99, inventory=3)
-
-        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer 0c56e6181c72efc15602be1706efa5c4ef4d0579')
+        Menu.objects.create(title="Dish2", price=10.99, inventory=2)
         self.url = reverse('menu-list')
 
-    def test_getall(self):
+    def test_getall_authenticated(self):
         response = self.client.get(self.url)
-        menus = Menu.objects.all()
-        serializer = MenuSerializer(menus, many=True)
+        #print(response.content)  # Print the response content
+        last_menu = Menu.objects.last()
+        serializer = MenuSerializer([last_menu], many=True)
 
-        self.assertEqual(response.status_code, status.HTTP_401_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
